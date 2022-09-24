3b0eaf4c3fe3e61b1b5b3218e7e4b6568aed638c
mutaiallan233 <mutaiallan233@gmail.com>
2022-09-24 11:34:51 +0300

    1st commit

---
+++ lib/main.dart
@@ -0,0 +1,95 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
+import 'package:railsv2/firebase_options.dart';
+import 'package:railsv2/map_detail/map_page.dart';
+
+void main() async {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const TheMap(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => const TheMap()),
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    initialization();
+  }
+
+  void initialization() async {
+    print('ready in 3...');
+    await Future.delayed(const Duration(seconds: 1));
+    print('ready in 2...');
+    await Future.delayed(const Duration(seconds: 1));
+    print('ready in 1...');
+    await Future.delayed(const Duration(seconds: 1));
+    print('go!');
+    FlutterNativeSplash.remove();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
---
+++ lib/map_detail/map_page.dart
@@ -0,0 +1,171 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
+import 'package:flutter_polyline_points/flutter_polyline_points.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+
+class TheMap extends StatefulWidget {
+  const TheMap({Key? key}) : super(key: key);
+
+  @override
+  State<TheMap> createState() => _TheMapState();
+}
+
+class _TheMapState extends State<TheMap> {
+  final Completer<GoogleMapController> _controller = Completer();
+
+  static const LatLng sourceLocation = LatLng(0.064611, 37.6268218);
+  static const LatLng destination = LatLng(0.111551, 37.601608);
+
+  GoogleMapController? mapController; //contrller for Google map
+
+  //--//
+  //REAL-TIME LOCATION
+  //
+  //Position? currentPosition;
+
+  /*void getCurrentLocation() async {
+    Location location =  Location();
+    location.getLocation().then(
+          (location) {
+            print(location);
+        currentLocation = location;
+      },
+    );
+    GoogleMapController googleMapController = await _controller.future;
+    location.onLocationChanged.listen(
+          (newLoc) {
+        currentLocation = newLoc;
+        googleMapController.animateCamera(
+          CameraUpdate.newCameraPosition(
+            CameraPosition(
+              //zoom: 13.5,
+              target: LatLng(
+                newLoc.latitude!,
+                newLoc.longitude!,
+              ),
+            ),
+          ),
+        );
+        setState(() {});
+      },
+    );
+  }*/
+  //--//
+
+  //--//
+  //ADDING POLYLINE DETAILS
+  //
+
+  List<LatLng> polylineCoordinates = [];
+  void getPolyPoints() async {
+    PolylinePoints polylinePoints = PolylinePoints();
+    PolylineResult result = await polylinePoints.getRouteBetweenCoordinates(
+      "AIzaSyAcgGBRwQ1KjnoV4-4eVzMRNT0bFgkLFKQ", // Your Google Map Key
+      PointLatLng(sourceLocation.latitude, sourceLocation.longitude),
+      PointLatLng(destination.latitude, destination.longitude),
+    );
+    if (result.points.isNotEmpty) {
+      result.points.forEach(
+            (PointLatLng point) => polylineCoordinates.add(
+          LatLng(point.latitude, point.longitude),
+        ),
+      );
+      setState(() {});
+    }
+  }
+  //--//
+  //GET CURRENT POSITION
+  //
+  Position? currentPosition;
+
+  void _getCurrentPosition() async {
+    await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high).then((Position position) async {
+      setState(() {
+        currentPosition = position;
+        print('CURRENT POSITION IS:  $currentPosition');
+
+        mapController!.animateCamera(
+          CameraUpdate.newCameraPosition(
+            CameraPosition(
+              target: LatLng(position.latitude, position.longitude),
+              zoom: 18.0,
+            ),
+          ),
+        );
+
+      });
+      //await _getAddress();
+    }).catchError((e) {
+      print(e);
+    });
+  }
+
+
+  //--//
+  //map details on init state
+  //
+  void initialization() async {
+    print('ready in 3...');
+    await Future.delayed(const Duration(seconds: 1));
+    print('ready in 2...');
+    await Future.delayed(const Duration(seconds: 1));
+    print('ready in 1...');
+    await Future.delayed(const Duration(seconds: 1));
+    print('go!');
+    FlutterNativeSplash.remove();
+  }
+  @override
+  void initState() {
+    initialization();
+     getPolyPoints();
+    super.initState();
+  }
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('TheMap'),
+        backgroundColor: Colors.redAccent,
+      ),
+      body: currentPosition == null
+          ? const Center(child: Text("Loading"))
+          : GoogleMap(
+        initialCameraPosition: CameraPosition(
+          target: LatLng(
+              currentPosition!.latitude!, currentPosition!.longitude!),
+          zoom: 13.5,
+        ),
+        markers: {
+          Marker(
+            markerId: const MarkerId("currentLocation"),
+            position: LatLng(
+                currentPosition!.latitude!, currentPosition!.longitude!),
+          ),
+          const Marker(
+            markerId: MarkerId("source"),
+            position: sourceLocation,
+          ),
+          const Marker(
+            markerId: MarkerId("destination"),
+            position: destination,
+          ),
+        },
+        onMapCreated: (mapController) {
+          _controller.complete(mapController);
+        },
+        polylines: {
+          Polyline(
+            polylineId: const PolylineId("route"),
+            points: polylineCoordinates,
+            color: const Color(0xFF7B61FF),
+            width: 6,
+          ),
+        },
+      ),
+    );
+  }
+}
